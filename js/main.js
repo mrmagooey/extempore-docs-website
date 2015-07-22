0972df8e2aa75bcd6518e95b9551a4c4b139ce2b
mrmagooey <peter@Peters-MacBook-Pro.local>
2015-07-22 17:13:34 +1000

    First working documentation system

---
+++ js/main.js
@@ -0,0 +1,104 @@
+
+var DocumentBox = React.createClass({displayName: "DocumentBox",
+    getInitialState: function() {
+        return {
+            data: [],
+            currentData: [],
+            fullData: [],
+        };
+    },
+    
+    componentDidMount: function() {
+        var _this = this;
+        var request = new XMLHttpRequest();
+        request.open('GET', this.props.url, true);
+        
+        request.onload = function(data) {
+            var jsonData = JSON.parse(request.responseText);
+            var sortedJsonData = _.sortBy(jsonData, function(a){
+                return a.name.toLowerCase();
+            });
+            _this.setState({fullData:sortedJsonData});
+            _this.setState({currentData:sortedJsonData});
+            window.test = jsonData;
+        };
+        request.send();
+    },
+    
+    handleSearchTerm: function(newTerm) {
+        var filteredData = _.filter(this.state.fullData, function(a){
+            return _.startsWith(a.name, newTerm)
+        });
+        this.setState({currentData:filteredData});
+    },
+    
+    render: function(){
+        console.log(this.state.currentData.length);
+        return (
+            React.createElement("div", {className: "documentBox"}, 
+            React.createElement("h1", null, "Documentation"), 
+            React.createElement(SearchForm, {onSearchTerm: this.handleSearchTerm}), 
+            React.createElement(DocumentList, {data: this.state.currentData})
+            )
+        );
+    }
+});
+
+var DocumentList = React.createClass({displayName: "DocumentList",
+    render: function() {
+        var docItems = this.props.data.map(function(doc) {
+            return (
+                React.createElement(DocumentItem, {
+                key: doc.category + doc.name, 
+                category: doc.category, 
+                name: doc.name, 
+                args: doc.args, 
+                type: doc.type, 
+                docstring: doc.docstring
+                })
+            );
+        });
+        return (
+            React.createElement("div", {className: "documentList"}, 
+            docItems
+            )
+        );
+    }
+})
+
+
+var DocumentItem = React.createClass({displayName: "DocumentItem",
+    render: function(){
+        return (
+            React.createElement("div", {className: "documentItem"}, 
+            React.createElement("h2", {className: "documentName"}, this.props.name), 
+            React.createElement("p", null, " ", this.props.category, " "), 
+            React.createElement("p", null, " ", this.props.args, " "), 
+            React.createElement("p", null, " ", this.props.type, " "), 
+            React.createElement("p", null, " ", this.props.docstring, " ")
+            )
+        );
+    }
+})
+
+var SearchForm = React.createClass({displayName: "SearchForm",
+    
+    render: function() {
+        return (
+            React.createElement("div", {className: "form-group"}, 
+            React.createElement("form", {className: "searchForm"}, 
+            React.createElement("input", {type: "text", ref: "term", className: "form-control", placeholder: "Search", onKeyUp: this.handleKeyPress})
+            )
+            )
+        )
+    },
+    handleKeyPress: function(evt){
+        var searchTerm = React.findDOMNode(this.refs.term).value;
+        this.props.onSearchTerm(searchTerm);
+    }
+})
+
+React.render(
+    React.createElement(DocumentBox, {url: "xtmdoc.json"}),
+    document.getElementById('content')
+)
---
+++ jsx/main.jsx
@@ -0,0 +1,104 @@
+
+var DocumentBox = React.createClass({
+    getInitialState: function() {
+        return {
+            data: [],
+            currentData: [],
+            fullData: [],
+        };
+    },
+    
+    componentDidMount: function() {
+        var _this = this;
+        var request = new XMLHttpRequest();
+        request.open('GET', this.props.url, true);
+        
+        request.onload = function(data) {
+            var jsonData = JSON.parse(request.responseText);
+            var sortedJsonData = _.sortBy(jsonData, function(a){
+                return a.name.toLowerCase();
+            });
+            _this.setState({fullData:sortedJsonData});
+            _this.setState({currentData:sortedJsonData});
+            window.test = jsonData;
+        };
+        request.send();
+    },
+    
+    handleSearchTerm: function(newTerm) {
+        var filteredData = _.filter(this.state.fullData, function(a){
+            return _.startsWith(a.name, newTerm)
+        });
+        this.setState({currentData:filteredData});
+    },
+    
+    render: function(){
+        console.log(this.state.currentData.length);
+        return (
+            <div className="documentBox">
+            <h1>Documentation</h1>
+            <SearchForm onSearchTerm={this.handleSearchTerm}/>
+            <DocumentList data={this.state.currentData}/>
+            </div>
+        );
+    }
+});
+
+var DocumentList = React.createClass({
+    render: function() {
+        var docItems = this.props.data.map(function(doc) {
+            return (
+                <DocumentItem 
+                key={doc.category + doc.name}
+                category={doc.category}
+                name={doc.name}
+                args={doc.args}
+                type={doc.type}
+                docstring={doc.docstring}
+                ></DocumentItem>
+            );
+        });
+        return (
+            <div className="documentList">
+            {docItems}
+            </div>
+        );
+    }
+})
+
+
+var DocumentItem = React.createClass({
+    render: function(){
+        return (
+            <div className="documentItem">
+            <h2 className="documentName">{this.props.name}</h2>
+            <p> {this.props.category} </p>
+            <p> {this.props.args} </p>
+            <p> {this.props.type} </p>
+            <p> {this.props.docstring} </p>
+            </div>
+        );
+    }
+})
+
+var SearchForm = React.createClass({
+    
+    render: function() {
+        return (
+            <div className="form-group">
+            <form className="searchForm">
+            <input type="text" ref="term" className="form-control" placeholder="Search" onKeyUp={this.handleKeyPress}></input>
+            </form>
+            </div>
+        )
+    },
+    handleKeyPress: function(evt){
+        var searchTerm = React.findDOMNode(this.refs.term).value;
+        this.props.onSearchTerm(searchTerm);
+    }
+})
+
+React.render(
+    <DocumentBox url="xtmdoc.json"/>,
+    document.getElementById('content')
+)
