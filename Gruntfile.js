609b74a96ab7f796d3a7af65c8b68dae202b7a75
mrmagooey <peter@Peters-MacBook-Pro.local>
2015-07-23 17:27:39 +1000

    add modernizr to grunt, remove explicit reference in index.html

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,4 @@
 
-
 module.exports = function(grunt) {
     
     require('load-grunt-tasks')(grunt); 
@@ -16,6 +15,7 @@ module.exports = function(grunt) {
                 src: [
                     "bower_components/lodash/lodash.min.js",
                     "bower_components/react/react.js",
+                    'bower_components/modernizr/modernizr.js'
                 ],
                 dest: 'js/third-party.js'
             }
